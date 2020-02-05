8becfeb8b0a3c59d19b3d11eed4f45dd0f5f483d
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-05 22:42:23 +0100

    Display Jokes

--- session_2/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.chucknorrisjokes
 
-class JokeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class JokeAdapter(private val jokes: List<String>) :
+    RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
+
+    class JokeViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+        /*val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.joke_card, parent, false) as TextView*/
+
+        return JokeViewHolder(TextView(parent.context))
+    }
+    override fun getItemCount(): Int {
+        return jokes.size
+    }
+
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        holder.textView.text = jokes[position]
+    }
+
+
 }
--- session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
@@ -17,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewManager = LinearLayoutManager(this)
-        //viewAdapter = Jokesdapter(ChuckJokes.jokes)
+        viewAdapter = JokeAdapter(ChuckJokes.jokes)
+
+        joke_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter }
 
 
         ChuckJokes.jokes.forEach{Log.wtf("Joke", it)}
