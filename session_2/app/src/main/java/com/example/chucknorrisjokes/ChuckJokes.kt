c0454d2d1bb9cc6f7917a4ad965305211b52583f
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-05 18:29:38 +0100

    create a list of jokes

--- session_2/app/src/main/java/com/example/chucknorrisjokes/ChuckJokes.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/ChuckJokes.kt
@@ -1,5 +1,16 @@
 package com.example.chucknorrisjokes
 
 class ChuckJokes{
-
+    companion object{
+        val jokes = listOf<String>("A woman wanted to commit suicide so she gave Chuck Norris a blow job.",
+            "Chuck Norris once had a wrestling match with a condom. The condom tapped out.",
+            "Chuck Norris once encountered a man drinking a XX beer. Since then the man has been known as the most interesting man in the world because Chuck let him live.",
+            "All ten of Chuck Norris' fingers are middle fingers.",
+            "When Chuck Norris watches SpongeBob SquarePants, SpongeBob doesn't laugh.",
+            "Chuck Norris doesn't get frostbite, Chuck Norris bites the frost.",
+            "One time when Chuck Norris got wasted, he found a parked semi-trailer. He pissed in the gas-tank - wich of course was empty - and he filled it just for fun. The semi-trailer is now better known as Optimus Prime.",
+            "Those aren't credits that roll after Walker Texas Ranger. It is actually a list of fatalities that occurred during the making of the episode.",
+            "Under Armour asked Chuck Norris to protect their house",
+            "Peter Pan once woke-up Chuck Norris at 5:00 AM in the morning. As a result, Cuck Norris made Pancakes for breakfast.")
+    }
 }
--- session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ChuckJokes.jokes.forEach{Log.wtf("Joke", it)}
     }
 }
