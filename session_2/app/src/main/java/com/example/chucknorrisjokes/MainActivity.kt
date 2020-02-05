08ca6e4382b3e359c2632e94412b2233d373d464
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-05 19:26:43 +0100

    Create LinearLayoutManager

--- session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.chucknorrisjokes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        //viewAdapter = JokesAdapter(ChuckJokes.jokes)
+
 
         ChuckJokes.jokes.forEach{Log.wtf("Joke", it)}
     }
