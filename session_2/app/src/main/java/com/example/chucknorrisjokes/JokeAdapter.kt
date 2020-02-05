e97e7e36c7674398b877a85cb7bf456f96d6e3ca
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-05 23:26:55 +0100

    Create joke Card

--- session_2/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -2,26 +2,26 @@ package com.example.chucknorrisjokes
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.joke_layout.view.*
 
 class JokeAdapter(private val jokes: List<String>) :
     RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
 
-    class JokeViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class JokeViewHolder(val linearLayout: LinearLayout) : RecyclerView.ViewHolder(linearLayout)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.joke_layout, parent, false) as TextView
-
-        return JokeViewHolder(textView)
+        val linearLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.joke_layout, parent, false) as LinearLayout
+        return JokeViewHolder(linearLayout)
     }
     override fun getItemCount(): Int {
         return jokes.size
     }
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-        holder.textView.text = jokes[position]
+        holder.linearLayout.joke_text.text = jokes[position]
     }
 
 
