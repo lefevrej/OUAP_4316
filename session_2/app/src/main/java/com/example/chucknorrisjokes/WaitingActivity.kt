02b3d1515ca5f7adad71790e3f40fa868b911dab
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-26 09:22:02 +0100

    Add waiting activity for fun

---
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/WaitingActivity.kt
@@ -0,0 +1,23 @@
+package com.example.chucknorrisjokes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class WaitingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting)
+
+        val runnable = Runnable {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val handler = Handler()
+        handler.postDelayed(runnable, 2000)
+
+    }
+}
