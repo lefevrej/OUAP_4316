ebb7adba5a6e7fbad6a3d5bd97279d882337dfaa
lefevrej <josselin.lefevre@edu.esiee.fr>
2020-02-27 09:40:10 +0100

    Add Joke button

--- session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ session_2/app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -2,20 +2,20 @@ package com.example.chucknorrisjokes
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewAdapter: JokeAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,18 +30,26 @@ class MainActivity : AppCompatActivity() {
             adapter = viewAdapter
         }
 
+        val jokes: MutableList<Joke> = mutableListOf()
+
         val service = JokeApiServiceFactory().createService()
         val singleJoke: Single<Joke> = service.giveMeAJoke()
-        singleJoke
-            .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.io())
-            .subscribeBy(
-                onError = { e -> Log.wtf("Request", e) },
-                onSuccess = { joke: Joke -> Log.wtf("Joke", joke.value) }
-            )
 
-        val jokes: List<Joke> =
-            ChuckJokes.jokes.map { Json(JsonConfiguration.Stable).parse(Joke.serializer(), it) }
-        viewAdapter.setData(jokes)
+        button.setOnClickListener {
+            compositeDisposable.add(singleJoke
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onError = { e -> Log.wtf("Request", e) },
+                    onSuccess = { joke: Joke ->
+                        jokes.add(joke)
+                        Log.wtf("Request", joke.value)
+                        viewAdapter.setData(jokes)
+                    }
+                )
+            )
+        }
+
+        //compositeDisposable.dispose()
     }
 }
